42799d2b8736859ad12e6269013f16d4cd5f21c0
Herdiantri Sufriyana <herdiantrisufriyana@gmail.com>
2020-11-21 21:42:18 +0800

    Version being pushed to Bioconductor

---
+++ R/TidySet.read-function.R
@@ -0,0 +1,300 @@
+#' Read a .ts.tar.gz file to a TidySet
+#'
+#' This function read multiple files archived by tar with gzip compression
+#' to a TidySet.
+#'
+#' @param path A character of .ts.tar.gz file path (include file extension).
+#'
+#' @return output A TidySet, an ExpressionSet with three tables. Function of
+#' \code{TidySet.write} can write this file from the TidySet.
+#'
+#' @keywords .ts.tar.gz, TidySet
+#'
+#' @export
+#'
+#' @examples
+#'
+#' ## Create input example
+#' input=utils.example()
+#'
+#' ## Create a TidySet
+#' tidy_set=
+#'   TidySet.create(
+#'     value=input$value
+#'     ,outcome=input$outcome
+#'     ,similarity=input$similarity
+#'     ,mapping=input$mapping
+#'     ,ontology=input$ontology
+#'   )
+#'
+#' ## Write a .ts.tar.gz file from a TidySet
+#' TidySet.write(tidy_set,'example')
+#'
+#' ## Read a .ts.tar.gz file to a TidySet
+#' TidySet.read('example.ts.tar.gz')
+
+TidySet.read=function(path){
+
+  filename=path
+  path=str_remove_all(filename,'.ts.tar.gz')
+  dir.create(path)
+  untar(filename,exdir=path)
+
+  others=
+    readLines(paste0(path,'/others.txt')) %>%
+    lapply(X=1,other=.,function(X,other){
+      elements=
+        other %>%
+        str_detect('^>>') %>%
+        which()
+
+      elements %>%
+        lapply(
+          X=seq(length(.))
+          ,Y=.
+          ,Z=seq(length(other))
+          ,function(X,Y,Z){
+            if(X<length(Y)){
+              Z %>% .[.>Y[X] & .<Y[X+1]]
+            }else{
+              Z %>% .[.>Y[X]]
+            }
+          }
+        ) %>%
+        lapply(
+          X=seq(length(.))
+          ,Y=.
+          ,Z=other
+          ,function(X,Y,Z){
+            paste(Z[Y[[X]]],collapse=' ')
+          }
+        ) %>%
+        setNames(str_remove_all(other[elements],'>>'))
+    }) %>%
+    .[[1]]
+
+  adata=
+    paste0(path,'/exprs.csv') %>%
+    read_csv(
+      col_names=str_split(others$sampleNames,'\\s')[[1]]
+      ,col_types=
+        str_split(others$sampleNames,'\\s')[[1]] %>%
+        length() %>%
+        rep(x='d',times=.) %>%
+        setNames(str_split(others$sampleNames,'\\s')[[1]])
+    ) %>%
+    as.data.frame() %>%
+    `rownames<-`(str_split(others$featureNames,'\\s')[[1]]) %>%
+    as.matrix()
+
+  pdata=
+    paste0(path,'/pData.csv') %>%
+    read_csv(
+      col_names=str_split(others$varLabels,'\\s')[[1]]
+      ,col_types=
+        str_split(others$varClass,'\\s')[[1]] %>%
+        sapply(function(x){
+          case_when(
+            x=='numeric'~'d'
+            ,x=='integer'~'i'
+            ,x=='factor'~'f'
+            ,x=='logical'~'l'
+            ,TRUE~'c'
+          )
+        }) %>%
+        setNames(str_split(others$varLabels,'\\s')[[1]])
+    ) %>%
+    as.data.frame()
+
+  pdata=
+    pdata %>%
+    mutate(
+      outcome=
+        setNames(
+          pdata$outcome
+          ,str_split(others$sampleNames,'\\s')[[1]]
+        )
+    ) %>%
+    `rownames<-`(str_split(others$sampleNames,'\\s')[[1]]) %>%
+    `attributes<-`(attributes(.)[c('names','row.names','class')])
+
+  pdata=
+    pdata %>%
+    AnnotatedDataFrame(
+      varMetadata=
+        str_split(others$varMetadata,'\\s')[[1]] %>%
+        data.frame(labelDescription=.) %>%
+        mutate(
+          labelDescription=
+            ifelse(labelDescription=='NA',NA,labelDescription)
+        ) %>%
+        `rownames<-`(str_split(others$varLabels,'\\s')[[1]])
+    )
+
+  fdata=
+    paste0(path,'/fData.csv') %>%
+    read_csv(
+      col_names=str_split(others$fvarLabels,'\\s')[[1]]
+      ,col_types=
+        str_split(others$fvarClass,'\\s')[[1]] %>%
+        sapply(function(x){
+          case_when(
+            x=='numeric'~'d'
+            ,x=='integer'~'i'
+            ,x=='factor'~'f'
+            ,x=='logical'~'l'
+            ,TRUE~'c'
+          )
+        }) %>%
+        setNames(str_split(others$fvarLabels,'\\s')[[1]])
+    ) %>%
+    as.data.frame() %>%
+    `rownames<-`(str_split(others$featureNames,'\\s')[[1]]) %>%
+    `attributes<-`(attributes(.)[c('names','row.names','class')])
+
+  similarity=
+    paste0(path,'/similarity.csv') %>%
+    read_csv(
+      col_names=str_split(others$simNames,'\\s')[[1]]
+      ,col_types=
+        str_split(others$simNames,'\\s')[[1]] %>%
+        length() %>%
+        rep(x='d',times=.) %>%
+        setNames(str_split(others$simNames,'\\s')[[1]])
+    ) %>%
+    as.data.frame() %>%
+    `rownames<-`(str_split(others$simNames,'\\s')[[1]]) %>%
+    as.matrix()
+
+  ontomap=
+    adata %>%
+    t() %>%
+    array(
+      dim=
+        c(dim(.)[1]
+          ,colnames(.) %>%
+            lapply(str_split_fixed,'x|y|z',4) %>%
+            sapply(as.integer) %>%
+            t() %>%
+            .[,2:4] %>%
+            colMaxs()
+        )
+      ,dimnames=list(rownames(.),NULL,NULL,NULL)
+    )
+
+  ontotype=
+    fdata %>%
+    lapply(X=seq(ncol(.)-1),Y=.,function(X,Y){
+      Z=Y %>%
+        select(-feature) %>%
+        .[,X,drop=F] %>%
+        rownames_to_column(var='pos_id') %>%
+        setNames(c('pos_id','ontotype')) %>%
+        filter(ontotype==1) %>%
+        left_join(
+          rownames_to_column(Y,var='pos_id') %>%
+            select(pos_id,feature),by='pos_id'
+        )
+
+      K=Z %>%
+        pull(pos_id) %>%
+        str_split_fixed('x|y|z',4) %>%
+        .[,2:4]
+
+      matrix(
+        as.integer(K)
+        ,ncol=3
+        ,byrow=F
+        ,dimnames=list(Z$feature,c('x','y','z'))
+      )
+    }) %>%
+    setNames(fdata %>% colnames(.) %>% .[.!='feature']) %>%
+    c(list(
+      root=
+        fdata  %>%
+        rownames_to_column(var='pos_id') %>%
+        select(pos_id,feature) %>%
+        filter(!is.na(feature)) %>%
+        lapply(X=1,Y=.,function(X,Y){
+          Z=Y %>%
+            pull(pos_id) %>%
+            str_split_fixed('x|y|z',4) %>%
+            .[,2:4]
+          matrix(
+            as.integer(Z)
+            ,ncol=3
+            ,byrow=F
+            ,dimnames=list(Y$feature,c('x','y','z'))
+          )
+        }) %>%
+        .[[1]]
+    ))
+
+  fdata=
+    fdata %>%
+    AnnotatedDataFrame(
+      varMetadata=
+        str_split(others$fvarMetadata,'\\s')[[1]] %>%
+        data.frame(labelDescription=.) %>%
+        mutate(
+          labelDescription=
+            ifelse(labelDescription=='NA',NA,labelDescription)
+        ) %>%
+        `rownames<-`(str_split(others$fvarLabels,'\\s')[[1]])
+    )
+
+  ontology=
+    paste0(path,'/ontology.csv') %>%
+    read_csv(
+      col_names=str_split(others$ontoNames,'\\s')[[1]]
+      ,col_types=
+        str_split(others$ontoClass,'\\s')[[1]] %>%
+        sapply(function(x){
+          case_when(
+            x=='numeric'~'d'
+            ,x=='integer'~'i'
+            ,x=='factor'~'f'
+            ,x=='logical'~'l'
+            ,TRUE~'c'
+          )
+        }) %>%
+        setNames(str_split(others$ontoNames,'\\s')[[1]])
+    ) %>%
+    as.data.frame() %>%
+    `attributes<-`(attributes(.)[c('names','row.names','class')])
+
+  xData=
+    new(
+      'MIAME'
+      ,name=others$name
+      ,lab=others$lab
+      ,contact=others$contact
+      ,title=others$title
+      ,abstract=others$abstract
+      ,url=others$url
+      ,pubMedIds=others$pubMedIds
+      ,other=
+        list(
+          similarity=similarity
+          ,ontomap=ontomap
+          ,ontotype=ontotype
+          ,ontology=ontology
+        )
+    )
+
+  unlink(path,T)
+
+  eset=
+    ExpressionSet(
+      assayData=adata
+      ,phenoData=pdata
+      ,featureData=fdata
+      ,experimentData=xData
+    )
+
+  if(paste0(str_split(others$annotation,'\\s')[[1]],collapse='')==''){
+    annotation(eset)=''
+  }
+
+  eset
+}
---
+++ R/generator.ontonet-function.R
@@ -0,0 +1,498 @@
+#' Make an ontonet generator for visible neural network (VNN) modeling
+#'
+#' This function create a function that generate a Keras Convolutional Neural
+#' Network (CNN) model with a specific layer architecture for each path in the
+#' hierarchy of the given ontology.
+#'
+#' @param tidy_set TidySet, an ExpressionSet with three tables.
+#' @param path A character of file path if the model json file is saved.
+#' @param init_seed An integer of random seed for ReLU initializer.
+#' @param init2_seed An integer of random seed for tanh initializer.
+#'
+#' @return output Keras model object, a pointer to Keras model object in python
+#' environment, which will be an input to train VNN model using Keras R package.
+#'
+#' @keywords ontonet, Keras model object
+#'
+#' @export
+#'
+#' @examples
+#'
+#' ## Create input example
+#' input=utils.example()
+#'
+#' ## Create a TidySet
+#' tidy_set=
+#'   TidySet.create(
+#'     value=input$value
+#'     ,outcome=input$outcome
+#'     ,similarity=input$similarity
+#'     ,mapping=input$mapping
+#'     ,ontology=input$ontology
+#'   )
+#'
+#' ## Create ontonet (Keras model object) generator function
+#' ontonet=generator.ontonet(tidy_set)
+
+generator.ontonet=function(tidy_set
+                           ,path=NULL
+                           ,init_seed=888
+                           ,init2_seed=9999){
+
+  # Recall ontomap
+  ontomap=
+    tidy_set %>%
+    experimentData() %>%
+    notes() %>%
+    .$ontomap
+
+  # Recall ontotype
+  ontotype=
+    tidy_set %>%
+    experimentData() %>%
+    notes() %>%
+    .$ontotype
+
+  # Recall ontology
+  ontology=
+    tidy_set %>%
+    experimentData() %>%
+    notes() %>%
+    .$ontology
+
+  # Build a function to insert an inception module along with a pre-activation residual unit
+  layer_inception_resnet=function(object
+                                  ,residue
+                                  ,filters
+                                  ,kernel_initializer
+                                  ,name=NULL){
+
+    pre_activation=object %>%
+      layer_batch_normalization(name=paste0(name,'_pre_bn')) %>%
+      layer_activation_relu(name=paste0(name,'_pre_ac'))
+
+    tower_1=pre_activation %>%
+      layer_separable_conv_2d(
+        filters=filters
+        ,kernel_size=c(1,1)
+        ,strides=c(1,1)
+        ,padding='same'
+        ,depthwise_initializer=kernel_initializer
+        ,pointwise_initializer=kernel_initializer
+        ,name=paste0(name,'_tower1_cv')
+      ) %>%
+      layer_batch_normalization(name=paste0(name,'_tower1_bn')) %>%
+      layer_activation_relu(name=paste0(name,'_tower1_ac'))
+
+    tower_2=pre_activation %>%
+      layer_max_pooling_2d(
+        pool_size=c(3,3)
+        ,strides=c(1,1)
+        ,padding='same'
+        ,name=paste0(name,'_tower2a_mp')
+      ) %>%
+      layer_separable_conv_2d(
+        filters=filters
+        ,kernel_size=c(1,1)
+        ,strides=c(1,1)
+        ,padding='same'
+        ,depthwise_initializer=kernel_initializer
+        ,pointwise_initializer=kernel_initializer
+        ,name=paste0(name,'_tower2b_cv')
+      ) %>%
+      layer_batch_normalization(name=paste0(name,'_tower2b_bn')) %>%
+      layer_activation_relu(name=paste0(name,'_tower2b_ac'))
+
+    tower_3a=pre_activation %>%
+      layer_separable_conv_2d(
+        filters=filters
+        ,kernel_size=c(1,1)
+        ,strides=c(1,1)
+        ,padding='same'
+        ,depthwise_initializer=kernel_initializer
+        ,pointwise_initializer=kernel_initializer
+        ,name=paste0(name,'_tower3a_cv')
+      ) %>%
+      layer_batch_normalization(name=paste0(name,'_tower3a_bn')) %>%
+      layer_activation_relu(name=paste0(name,'_tower3a_ac'))
+
+    tower_3b1=tower_3a%>%
+      layer_separable_conv_2d(
+        filters=filters
+        ,kernel_size=c(1,3)
+        ,strides=c(1,1)
+        ,padding='same'
+        ,depthwise_initializer=kernel_initializer
+        ,pointwise_initializer=kernel_initializer
+        ,name=paste0(name,'_tower3b1_cv')
+      ) %>%
+      layer_batch_normalization(name=paste0(name,'_tower3b1_bn')) %>%
+      layer_activation_relu(name=paste0(name,'_tower3b1_ac'))
+
+    tower_3b2=tower_3a%>%
+      layer_separable_conv_2d(
+        filters=filters
+        ,kernel_size=c(3,1)
+        ,strides=c(1,1)
+        ,padding='same'
+        ,depthwise_initializer=kernel_initializer
+        ,pointwise_initializer=kernel_initializer
+        ,name=paste0(name,'_tower3b2_cv')
+      ) %>%
+      layer_batch_normalization(name=paste0(name,'_tower3b2_bn')) %>%
+      layer_activation_relu(name=paste0(name,'_tower3b2_ac'))
+
+    tower_4b=pre_activation %>%
+      layer_separable_conv_2d(
+        filters=filters
+        ,kernel_size=c(1,1)
+        ,strides=c(1,1)
+        ,padding='same'
+        ,depthwise_initializer=kernel_initializer
+        ,pointwise_initializer=kernel_initializer
+        ,name=paste0(name,'_tower4a_cv')
+      ) %>%
+      layer_batch_normalization(name=paste0(name,'_tower4a_bn')) %>%
+      layer_activation_relu(name=paste0(name,'_tower4a_ac')) %>%
+      layer_separable_conv_2d(
+        filters=filters
+        ,kernel_size=c(3,3)
+        ,strides=c(1,1)
+        ,padding='same'
+        ,depthwise_initializer=kernel_initializer
+        ,pointwise_initializer=kernel_initializer
+        ,name=paste0(name,'_tower4b_cv')
+      ) %>%
+      layer_batch_normalization(name=paste0(name,'_tower4b_bn')) %>%
+      layer_activation_relu(name=paste0(name,'_tower4b_ac'))
+
+    tower_4c1=tower_4b%>%
+      layer_separable_conv_2d(
+        filters=filters
+        ,kernel_size=c(1,3)
+        ,strides=c(1,1)
+        ,padding='same'
+        ,depthwise_initializer=kernel_initializer
+        ,pointwise_initializer=kernel_initializer
+        ,name=paste0(name,'_tower4c1_cv')
+      ) %>%
+      layer_batch_normalization(name=paste0(name,'_tower4c1_bn')) %>%
+      layer_activation_relu(name=paste0(name,'_tower4c1_ac'))
+
+    tower_4c2=tower_4b %>%
+      layer_separable_conv_2d(
+        filters=filters
+        ,kernel_size=c(3,1)
+        ,strides=c(1,1)
+        ,padding='same'
+        ,depthwise_initializer=kernel_initializer
+        ,pointwise_initializer=kernel_initializer
+        ,name=paste0(name,'_tower4c2_cv')
+      ) %>%
+      layer_batch_normalization(name=paste0(name,'_tower4c2_bn')) %>%
+      layer_activation_relu(name=paste0(name,'_tower4c2_ac'))
+
+    towers=layer_concatenate(
+      c(tower_1
+        ,tower_2
+        ,tower_3b1,tower_3b2
+        ,tower_4c1,tower_4c2),
+      axis=-1,
+      name=paste0(name,'_dc')
+    )
+
+    scaling=towers%>%
+      layer_separable_conv_2d(
+        filters=dim(residue)[[4]]
+        ,kernel_size=c(1,1)
+        ,strides=c(1,1)
+        ,padding='same'
+        ,depthwise_initializer=kernel_initializer
+        ,pointwise_initializer=kernel_initializer
+        ,name=paste0(name,'_sc_cv')
+      )
+
+    inception_resnet=c(scaling,residue) %>%
+      layer_add(name=name)
+
+    inception_resnet
+  }
+
+  # Build a function to insert auxiliary output layers
+  layer_aux_output=function(object
+                            ,filters
+                            ,units
+                            ,kernel_initializer
+                            ,kernel_initializer2
+                            ,name=NULL){
+
+    object %>%
+      layer_average_pooling_2d(
+        pool_size=c(5,5)
+        ,strides=c(3,3)
+        ,padding='valid'
+        ,name=paste0(name,'_ap')
+      ) %>%
+
+      layer_separable_conv_2d(
+        filters=filters
+        ,kernel_size=c(1,1)
+        ,strides=c(1,1)
+        ,padding='same'
+        ,depthwise_initializer=kernel_initializer
+        ,pointwise_initializer=kernel_initializer
+        ,name=paste0(name,'_hl1_cv')
+      ) %>%
+      layer_batch_normalization(name=paste0(name,'_hl1_bn')) %>%
+      layer_activation_relu(name=paste0(name,'_hl1_ac')) %>%
+
+      layer_dense(
+        units=units
+        ,kernel_initializer=kernel_initializer
+        ,name=paste0(name,'_hl2_de')
+      ) %>%
+      layer_batch_normalization(name=paste0(name,'_hl2_bn')) %>%
+      layer_activation_relu(name=paste0(name,'_hl2_ac')) %>%
+
+      layer_dense(
+        units=units
+        ,kernel_initializer=kernel_initializer
+        ,name=paste0(name,'_hl3_de')
+      ) %>%
+      layer_batch_normalization(name=paste0(name,'_hl3_bn')) %>%
+      layer_activation_relu(name=paste0(name,'_hl3_ac')) %>%
+
+      layer_dense(
+        units=2
+        ,activation='tanh'
+        ,kernel_initializer=kernel_initializer2
+        ,name=paste0(name,'_ao_tn')
+      ) %>%
+      layer_batch_normalization(name=name)
+
+  }
+
+  # Build a function to insert output layers
+  layer_output=function(object
+                        ,units
+                        ,kernel_initializer
+                        ,kernel_initializer2
+                        ,name=NULL){
+
+    object %>%
+      layer_average_pooling_2d(
+        pool_size=c(7,7)
+        ,strides=c(1,1)
+        ,padding='valid'
+        ,name=paste0(name,'_ap')
+      ) %>%
+
+      layer_dense(
+        units=units
+        ,kernel_initializer=kernel_initializer
+        ,name=paste0(name,'_hl1_de')
+      ) %>%
+      layer_batch_normalization(name=paste0(name,'_hl1_bn')) %>%
+      layer_activation_relu(name=paste0(name,'_hl1_ac')) %>%
+
+      layer_dense(
+        units=2
+        ,activation='tanh'
+        ,kernel_initializer=kernel_initializer2
+        ,name=paste0(name,'_mo_tn')
+      ) %>%
+      layer_batch_normalization(name=name)
+
+  }
+
+  # Build a generator function to construct ontonet
+
+  k_clear_session()
+  init=initializer_he_uniform(seed=init_seed)
+  init2=initializer_glorot_uniform(seed=init2_seed)
+
+  feature=ontology
+  while(sum(str_detect(feature$source,'ONT'))>0){
+    feature=
+      feature %>%
+      lapply(X=1:nrow(.),Y=.,function(X,Y){
+        if(str_detect(Y$source[X],'ONT')){
+          Z=filter(Y,target==Y$source[X] & relation=='feature')
+          if(nrow(Z)>0){
+            data.frame(
+              source=Z$source
+              ,target=Y$target[X]
+              ,similarity=Y$similarity[X]
+              ,relation='feature'
+            )
+          }else{
+            Y[X,]
+          }
+        }else{
+          Y[X,]
+        }
+      }) %>%
+      do.call(rbind,.)
+  }
+
+  feature=
+    feature %>%
+    select(source,target) %>%
+    rbind(
+      select(.,source) %>%
+        .[!duplicated(.),,drop=F] %>%
+        mutate(target='root')
+    ) %>%
+    group_by(target) %>%
+    summarise(n=n()) %>%
+    ungroup() %>%
+    mutate(oid=str_replace_all(target,'ONT\\:','ONT')) %>%
+    select(oid,n)
+
+  hierarchy=
+    ontology %>%
+    filter(str_detect(source,'ONT\\:')) %>%
+    mutate(
+      from=str_replace_all(source,'ONT\\:','ONT')
+      ,to=str_replace_all(target,'ONT\\:','ONT')
+    ) %>%
+    select(from,to) %>%
+    rbind(
+      data.frame(
+        from=filter(.,!to%in%.$from)$to %>% .[!duplicated(.)]
+        ,to='root'
+      )
+    ) %>%
+    left_join(rename(feature,to=oid),by='to')
+
+  terminal_nodes=length(unique(hierarchy$from) %>% .[!.%in%hierarchy$to])
+  non_terminal_nodes=length(unique(hierarchy$to))
+
+  pb=startpb(0,5+terminal_nodes+non_terminal_nodes)
+  on.exit(closepb(pb))
+
+  setpb(pb,0)
+  inputs=
+    ontotype %>%
+    lapply(
+      X=seq(length(.))
+      ,Y=.
+      ,Z=ontomap
+      ,FUN=function(X,Y,Z){
+        layer_input(
+          shape=dim(Z)[2:4]
+          ,dtype='float32'
+          ,name=paste0(names(Y)[X],'_input')
+        )
+      }) %>%
+    setNames(names(ontotype))
+
+  hiddens=list()
+  outputs=list()
+
+  setpb(pb,1)
+  for(i in seq(terminal_nodes)){
+
+    setpb(pb,1+i)
+    A=unique(hierarchy$from) %>% .[!.%in%hierarchy$to] %>% .[i]
+    B=A
+    C=feature$n[feature$oid==A]
+
+    hiddens[[A]]=
+      layer_inception_resnet(
+        object=inputs[[B]]
+        ,residue=inputs[[A]]
+        ,filters=max(20,ceiling(0.3*C))
+        ,kernel_initializer=init
+        ,name=paste0(A,'_hidden')
+      )
+
+    outputs[[A]]=
+      hiddens[[A]] %>%
+      layer_aux_output(
+        filters=max(20,ceiling(0.3*C))
+        ,units=max(20,ceiling(0.3*C))
+        ,kernel_initializer=init
+        ,kernel_initializer2=init2
+        ,name=A
+      )
+  }
+  j=i
+
+  setpb(pb,2+j)
+  for(i in seq(non_terminal_nodes)){
+
+    setpb(pb,2+j+i)
+    A=unique(hierarchy$to)[i]
+    B=hierarchy$from[hierarchy$to==A]
+    C=feature$n[feature$oid==A]
+
+    if(length(B)==1){
+      hiddens[[A]]=
+        layer_inception_resnet(
+          object=hiddens[[B]]
+          ,residue=inputs[[A]]
+          ,filters=max(20,ceiling(0.3*C))
+          ,kernel_initializer=init
+          ,name=paste0(A,'_hidden')
+        )
+    }else{
+      hiddens[[A]]=
+        hiddens[B] %>%
+        unlist(use.names=F) %>%
+        layer_concatenate(
+          axis=-1
+          ,name=paste0(A,'_dc')
+        ) %>%
+        layer_inception_resnet(
+          object=.
+          ,residue=inputs[[A]]
+          ,filters=max(20,ceiling(0.3*C))
+          ,kernel_initializer=init
+          ,name=paste0(A,'_hidden')
+        )
+    }
+
+    if(A!=unique(hierarchy$to)[length(unique(hierarchy$to))]){
+      outputs[[A]]=
+        hiddens[[A]] %>%
+        layer_aux_output(
+          filters=max(20,ceiling(0.3*C))
+          ,units=max(20,ceiling(0.3*C))
+          ,kernel_initializer=init
+          ,kernel_initializer2=init2
+          ,name=A
+        )
+    }else{
+      outputs[[A]]=
+        hiddens[[A]] %>%
+        layer_output(
+          units=max(20,ceiling(0.3*C))
+          ,kernel_initializer=init
+          ,kernel_initializer2=init2
+          ,name=A
+        )
+    }
+
+  }
+  j=j+i
+
+  rm(i,A,B,C)
+
+  setpb(pb,3+j)
+  model=keras_model(inputs=inputs,outputs=outputs)
+
+  setpb(pb,4+j)
+  if(!is.null(path)){
+    model %>%
+      model_to_json() %>%
+      writeLines(paste0(path,'.json'))
+  }
+
+  setpb(pb,5+j)
+  k_clear_session()
+  if(!is.null(path)) cat('\nOntonet has been saved to',paste0(path,'.json'))
+
+  model
+
+}
