85c42e40126bb430a3ee98192066990a60605390
Herdiantri Sufriyana <herdiantrisufriyana@gmail.com>
2020-11-22 16:59:09 +0800

    Version being pushed to Biopython

--- divnn/generator.py
+++ divnn/generator.py
@@ -600,13 +600,13 @@ def ontoarray(TidySet,index,batch_size):
     x_array={}
     for k in ontofilter.keys():
       x_array[k]=ontomap[rows] * ontofilter[k]
-    # x_array=list(x_array.values())
+    x_array=list(x_array.values())
     
     y_vector={}
     for k in ontofilter.keys():
       y_vector[k]=list(operator.itemgetter(*rows)(outcome))
-    # y_vector=list(y_vector.values())
+    y_vector=list(y_vector.values())
     
-    batch={'inputs':x_array,'outputs':y_vector}
+    batch=(x_array,y_vector)
     
     yield(batch)
